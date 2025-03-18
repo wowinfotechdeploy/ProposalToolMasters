208094d106b8698e13ed329843940eefdd586a79
GaneshMali <wow.mongodbapi@gmail.com>
2025-03-18 09:56:58 +0530

    Base Url Chnage

--- src/Base-Url/Base_Url.jsx
+++ src/Base-Url/Base_Url.jsx
@@ -1,11 +1,11 @@
 //Vercel Api
-export const Base_Url = "https://proposal-vercel-one.mkisan.com/api"
+// export const Base_Url = "https://proposal-vercel-one.mkisan.com/api"
 // export const Base_Url = "https://proposal-vercelapi.mkisan.com/api"
 // export const Base_Url = "https://determined-easley.184-168-123-97.plesk.page/api"
 // export const Base_Url = "https://nifty-herschel.184-168-123-97.plesk.page/api"
 
 //Master Api
-// export const Base_Url = "https://api.master.proposal.ust.outbooks.com/api"
+export const Base_Url = "https://api.master.proposal.ust.outbooks.com/api"
 
 // //temporyBAseURl;
 // export const Base_Url = "https://angry-blackburn.101-53-133-129.plesk.page/api"
@@ -23,10 +23,10 @@ export const Base_Url = "https://proposal-vercel-one.mkisan.com/api"
 // export const redirectUri = "https://proposal-tool.vercel.app";
 //New Vercel redirect
 // export const redirectUri = "https://proposal-tool-one.vercel.app";
-export const redirectUri = "https://proposal-tool-dev.vercel.app";
+// export const redirectUri = "https://proposal-tool-dev.vercel.app";
 
 //Master redirect
-// export const redirectUri =  "https://master.proposal.outbooks.com";
+export const redirectUri = "https://master.proposal.outbooks.com";
 
 //Pre-Prod redirect
 // export const redirectUri =  "https://preprod.proposal.outbooks.com";
@@ -35,13 +35,16 @@ export const redirectUri = "https://proposal-tool-dev.vercel.app";
 // export const redirectUri =  "https://app.proposal.outbooks.com";
 // export const mergePdfApiUrl = "http://localhost:3001/mergeUserPdfs";
 // export const generatePdfUrl = "http://localhost:3001/generatePdf";
-// export const mergePdfApiUrl = "https://loving-wilson.101-53-133-129.plesk.page/mergeUserPdfs";
-// export const generatePdfUrl = "https://loving-wilson.101-53-133-129.plesk.page/generatePdf";
-// export const saveImage = "https://loving-wilson.101-53-133-129.plesk.page/save-image"
-export const mergePdfApiUrl = "https://api.pdfgenration.proposal.ust.mkisan.com/mergeUserPdfs";
-export const generatePdfUrl = "https://api.pdfgenration.proposal.ust.mkisan.com/generatePdf";
-export const saveImage = "https://api.pdfgenration.proposal.ust.mkisan.com/save-image"
 // export const saveImage = "http://localhost:3001/save-image";
+//new Dev Link
+
+export const mergePdfApiUrl = "https://proposaltoolnodejsapi.onrender.com/mergeUserPdfs";
+export const generatePdfUrl = "https://proposaltoolnodejsapi.onrender.com/generatePdf";
+export const saveImage = "https://proposaltoolnodejsapi.onrender.com/save-image";
+// export const mergePdfApiUrl = "https://proposal.pdfgenration.mkisan.com/mergeUserPdfs";
+// export const generatePdfUrl = "https://proposal.pdfgenration.mkisan.com/generatePdf";
+// export const saveImage = "https://proposal.pdfgenration.mkisan.com/save-image"
+//Production Link 
 // export const mergePdfApiUrl = "https://api.pdfgenration.proposal.ust.outbooks.com/mergeUserPdfs";
 // export const generatePdfUrl = "https://api.pdfgenration.proposal.ust.outbooks.com/generatePdf";
 // export const saveImage = "https://api.pdfgenration.proposal.ust.outbooks.com/save-image"
