3b37cee28b88fa95669db4367790573bdd6afbcf
Saurabh Ghodke <react.wowinfotech@gmail.com>
2026-01-16 11:03:44 +0530

    view prposal bugs

--- src/Base-Url/Base_Url.jsx
+++ src/Base-Url/Base_Url.jsx
@@ -3,19 +3,19 @@
 // export const Base_Url = "https://proposal-vercelapi.mkisan.com/api"
 // export const Base_Url = "https://determined-easley.184-168-123-97.plesk.page/api"
 // export const Base_Url = "https://nifty-herschel.184-168-123-97.plesk.page/api"
- 
+
 //Master Api
 export const Base_Url = "https://api.master.proposal.ust.outbooks.com/api";
- 
+
 // //temporyBAseURl;
 // export const Base_Url = "https://angry-blackburn.101-53-133-129.plesk.page/api"
- 
+
 //Pre-Prod Api
 // export const Base_Url = "https://api.preprod.proposal.ust.outbooks.com/api"
- 
+
 //Production Api
 // export const Base_Url = "https://api.app.proposal.ust.outbooks.com/api"
- 
+
 //Local redirect
 // export const redirectUri = "http://localhost:3000";
 //Client Vercel redirect
@@ -23,41 +23,46 @@ export const Base_Url = "https://api.master.proposal.ust.outbooks.com/api";
 //New Vercel redirect
 // export const redirectUri = "https://proposal-tool-one.vercel.app";
 // export const redirectUri = "https://proposal-tool-dev.vercel.app";
- 
+
 //Master redirect
 export const redirectUri = "https://master.proposal.outbooks.com";
- 
+
 //Pre-Prod redirect
 // export const redirectUri =  "https://preprod.proposal.outbooks.com";
- 
+
 //Production redirect
 // export const redirectUri =  "https://app.proposal.outbooks.com";
 // export const mergePdfApiUrl = "http://localhost:8080/mergeUserPdfs";
 // export const generatePdfUrl = "http://localhost:8080/generatePdf";
 // export const saveImage = "http://localhost:8080/save-image";
 //new Dev Link
- 
+
 // export const mergePdfApiUrl = "https://multipurpose.mkisan.com/mergeUserPdfs";
 // export const generatePdfUrl = "https://multipurpose.mkisan.com/generatePdf";
 // export const saveImage = "https://multipurpose.mkisan.com/save-image";
 // export const mergePdfApiUrl = "https://proposal.pdfapi.wowinfosolutions.com/mergeUserPdfs";
 // export const generatePdfUrl = "https://proposal.pdfapi.wowinfosolutions.com/generatePdf";
 // export const saveImage = "https://proposal.pdfapi.wowinfosolutions.com/save-image"
- 
+
+// export const mergePdfApiUrl =
+//   "https://master.proposal.wowinfosolutions.com/mergeUserPdfs";
+// export const generatePdfUrl =
+//   "https://master.proposal.wowinfosolutions.com/generatePdf";
+// export const saveImage =
+//   "https://master.proposal.wowinfosolutions.com/save-image";
 export const mergePdfApiUrl =
-  "https://master.proposal.wowinfosolutions.com/mergeUserPdfs";
+  "https://test.pdfgeneration.wowinfosolutions.com/mergeUserPdfs";
 export const generatePdfUrl =
-  "https://master.proposal.wowinfosolutions.com/generatePdf";
+  "https://test.pdfgeneration.wowinfosolutions.com/generatePdf";
 export const saveImage =
-  "https://master.proposal.wowinfosolutions.com/save-image";
+  "https://test.pdfgeneration.wowinfosolutions.com/save-image";
 //Production Link
 // export const mergePdfApiUrl = "https://api.pdfgenration.proposal.ust.outbooks.com/mergeUserPdfs";
 // export const generatePdfUrl = "https://api.pdfgenration.proposal.ust.outbooks.com/generatePdf";
 // export const saveImage = "https://api.pdfgenration.proposal.ust.outbooks.com/save-image"
 export const CompaniesHouseBase_Url = "https://api.companieshouse.gov.uk";
- 
+
 export const OldOutbooksBaseUrl = "https://proposal.outbooks.com/api";
- 
+
 export const AccessKeyBaseUrl =
   "https://api.api.master.proposal.ust.outbooks.com";
-  
